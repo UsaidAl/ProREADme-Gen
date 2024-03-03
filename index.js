abf7e89374701311392801e8ef4bc18d11fe3820
UsaidAl <ali_usaid98@hotmail.com>
2024-03-03 18:25:24 +0000

    Questions

--- index.js
+++ index.js
@@ -10,6 +10,48 @@ const questions = [
         name: 'title',
         message: 'What is your project title?',
       },
+
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a brief description of your project:'
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide the installation instructions:'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the use of this project?'
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please add any contributions:'
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Please enter any test instructions:'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for this application:',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD']
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter the email address:'
+      },
 ];
 
 // TODO: Create a function to write README file
